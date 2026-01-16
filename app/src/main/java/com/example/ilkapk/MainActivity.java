c21bd64942b7037af92ed120d6f89e83361f57e8
isegzidrafok <capo17926@gmail.com>
2026-01-16 18:05:33 +0300

    Add TextView with custom properties in MainActivity

--- app/src/main/java/com/example/ilkapk/MainActivity.java
+++ app/src/main/java/com/example/ilkapk/MainActivity.java
@@ -1,12 +1,23 @@
 package com.example.ilkapk;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        TextView text = new TextView(this);
+        text.setText("G");
+        text.setTextSize(120);
+        text.setTextColor(Color.YELLOW);
+        text.setGravity(Gravity.CENTER);
+
+        setContentView(text);
     }
 }
-
